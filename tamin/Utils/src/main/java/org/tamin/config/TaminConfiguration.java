0204ffed8142ceed8cab720766b58468cdad957f
s_zakipour@yahoo.com <s_zakipour@yahoo.com>
2016-01-14 13:21:49 +0330

    queue size config

---
+++ tamin/Utils/src/main/java/org/tamin/config/TaminConfiguration.java
@@ -0,0 +1,71 @@
+package org.tamin.config;
+
+import javax.servlet.ServletContext;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
+/**
+ * Created by s.zakipour on 01/13/2016.
+ */
+public class TaminConfiguration {
+
+    private TaminConfiguration () {
+    }
+
+    private static TaminConfiguration  _Configuration;
+
+    public static TaminConfiguration  getConfiguration() {
+        if (_Configuration == null) {
+            _Configuration = new TaminConfiguration ();
+            return _Configuration;
+        } else {
+            return _Configuration;
+        }
+    }
+
+
+    public long getQueueSize() {
+
+        Properties prop  = readfile();
+        if(prop!= null)
+        {
+            return Long.parseLong(prop.getProperty("queueSize"));
+        }
+        else
+        {
+            return  0;
+        }
+
+    }
+
+    private Properties readfile() {
+        Properties properties = new Properties();
+        OutputStream output = null;
+
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+            properties.load(classLoader.getResourceAsStream("config.properties"));
+
+            return properties;
+        } catch (IOException io) {
+            io.printStackTrace();
+            System.out.println(io.getMessage());
+        } finally
+        {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }
+        return properties;
+
+    }
+
+}
